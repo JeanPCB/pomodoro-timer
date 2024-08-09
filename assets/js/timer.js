ef4354fc02cadf139aac635153324935a0f9bb31
JeanPCB <jeanp.cebinel01@gmail.com>
2024-08-09 18:44:04 -0300

    mudança drástica: retirada dos botões pausa e reset

--- assets/js/timer.js
+++ assets/js/timer.js
@@ -1,31 +1,83 @@
 function timer() {
+    // Variables
     const time = document.querySelector("#time");
     let mins = document.querySelector(".minutes");
     let secs = document.querySelector(".seconds");
-    const startBtn = document.querySelector(".start-btn");
-    const stopBtn = document.querySelector(".stop-btn");
-    const resetBtn = document.querySelector(".reset-btn");
-
-    let seconds = minutesToSec(25);
-    let timer;
+    // const startBtn = document.querySelector(".start-btn");
+    // const stopBtn = document.querySelector(".stop-btn");
+    // const resetBtn = document.querySelector(".reset-btn");
     
-    startBtn.addEventListener("click", () => {
-        timer = setInterval(() => {
+    const standardTime = minutesToSec(25);
+    let seconds = standardTime;
+    let runTime = null;
+    
+    // Invoking
+    document.addEventListener("click", (e) => {
+        el = e.target;
+
+        if(el.classList.contains("start-btn")) {
+            start();
+            return;
+        }
+
+        if(el.classList.contains("stop-btn")) {
+            stop();
+            return;
+        }
+
+        if(el.classList.contains("work-btn")) {
+            reset();
+            return;
+        }
+
+        if(el.classList.contains("short-btn")) {
+            reset("shortBreak");
+            return;
+        }
+
+        if(el.classList.contains("long-btn")) {
+            reset("longBreak");
+            return;
+        }
+    })
+
+    // Functions
+    function start() {
+        runTime = setInterval(() => {
+            if(seconds == 0) return;
+
             seconds--;
             displayTime(seconds);
-        }, 1000)
-    });
+        }, 1000);
+    }
 
-    stopBtn.addEventListener("click", () => {
-        clearInterval(timer);
-    });
+    function stop() {
+        clearInterval(runTime);
+    }
 
-    resetBtn.addEventListener("click", () => {
-        clearInterval(timer);
-        resetTimeText(mins, "25");
-        resetTimeText(secs, "00");
-        seconds = minutesToSec(25);
-    });
+    function reset(type) {
+        clearInterval(runTime);
+
+        switch (type) {
+            case "shortBreak":
+                mins.innerText = "05";
+                secs.innerText = "00";
+                seconds = minutesToSec(5);
+                break;
+
+            case "longBreak":
+                mins.innerText = "15";
+                secs.innerText = "00";
+                seconds = minutesToSec(15);
+                break;
+            
+            default:
+                mins.innerText = "25";
+                secs.innerText = "00";
+                seconds = standardTime;
+                break;
+        }
+    }
 
     function minutesToSec(minutes) {
         return minutes * 60;
@@ -50,10 +102,6 @@ function timer() {
         } else {
             return timeUnit;
         }
-    }
-
-    function resetTimeText(el, startValue) {
-        el.innerText = startValue;
     }
 }
 
