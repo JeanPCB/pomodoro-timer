37ce21a12d47ab542f856d174f04f39d3ced864f
jeanpcb <jeanp.cebinel01@gmail.com>
2024-08-30 13:09:11 -0300

    adicao: som de click botao run

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,13 @@
 function main() {
+    const runBtn = document.querySelector(".run-btn");
+    const clickAudio = new Audio("../audio/mouse-click-sound-233951.mp3");
     const taskModal = document.querySelector(".task-modal-backdrop");
     const taskInput = document.querySelector(".task-input");
+
+    // Start/Stop button sound
+    runBtn.addEventListener("click", () => {
+        clickAudio.play();
+    });
 
     document.addEventListener("click", (e) => {
         const el = e.target;
